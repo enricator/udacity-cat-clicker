26fd2cfdd30375e826cd857260997dfcc0ea284f
Enrico Imbalzano <enrico.i69@gmail.com>
2017-11-16 15:48:17 +0100

    added localStorage functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   /* ### MODEL ### */
   var model = {
+    lsName: 'ucc-kitties',
     picSize: 350,
     dummyPlaceholder: '',
     kitties: [],
@@ -9,45 +10,50 @@ $(document).ready(function() {
 
     init: function() {
       this.dummyPlaceholder = "http://via.placeholder.com/" + this.picSize + "x" + this.picSize + "";
-      // cat pics taken from lorempixel.com
-      // ex: https://lorempixel.com/350/350/cats/7/
-      this.kitties= [
-        {
-          name: "Oscar",
-          clicks: 0,
-          url: "images/cat5.jpg"
-        },
-        {
-          name: "Wendy",
-          clicks: 0,
-          url: "images/cat4.jpg"
-        },
-        {
-          name: "Igor",
-          clicks: 0,
-          url: "images/cat3.jpg"
-        },
-        {
-          name: "Clara",
-          clicks: 0,
-          url: "images/cat1.jpg"
-        },
-        {
-          name: "Flash",
-          clicks: 0,
-          url: "images/cat7.jpg"
-        },
-        {
-          name: "Dori",
-          clicks: 0,
-          url: "images/cat9.jpg"
-        },
-        {
-          name: "Georg",
-          clicks: 0,
-          url: "images/cat8.jpg"
-        }
-      ];
+      if (localStorage.getItem(this.lsName)) {
+        this.kitties = JSON.parse(localStorage.getItem(this.lsName));
+      } else {
+        // cat pics taken from lorempixel.com
+        // ex: https://lorempixel.com/350/350/cats/7/
+        this.kitties= [
+          {
+            name: "Oscar",
+            clicks: 0,
+            url: "images/cat5.jpg"
+          },
+          {
+            name: "Wendy",
+            clicks: 0,
+            url: "images/cat4.jpg"
+          },
+          {
+            name: "Igor",
+            clicks: 0,
+            url: "images/cat3.jpg"
+          },
+          {
+            name: "Clara",
+            clicks: 0,
+            url: "images/cat1.jpg"
+          },
+          {
+            name: "Flash",
+            clicks: 0,
+            url: "images/cat7.jpg"
+          },
+          {
+            name: "Dori",
+            clicks: 0,
+            url: "images/cat9.jpg"
+          },
+          {
+            name: "Georg",
+            clicks: 0,
+            url: "images/cat8.jpg"
+          }
+        ];
+        localStorage.setItem(this.lsName, JSON.stringify(this.kitties));
+      }
       this.activeKitty = 0;
     }
   };
@@ -58,33 +64,49 @@ $(document).ready(function() {
 
     init: function() {
       this.catList = $("#kittylist");
+      this.resetButton = $("#btnResetVotes");
       this.kitties = octopus.getKitties();
       this.show();
     },
 
     show: function() {
-      this.catList.innerHTML = '';
-      for (var i = 0; i < this.kitties.length; i++) {
-        this.catList.append(
-          '<li class="list-group-item kitty" id="kitty' +
-            i +
-            '"><span class="badge">' +
-            this.kitties[i].clicks +
-            "</span>" +
-            this.kitties[i].name +
-            "</li>"
-        );
-      }
+      this.catList.html('');
+      if (this.kitties.length == 0) {
+        this.catList.append('<p>no cats found</p>');
+      } else {
+        for (var i = 0; i < this.kitties.length; i++) {
+          this.catList.append(
+            '<li class="list-group-item kitty" id="kitty' +
+              i +
+              '"><span class="badge">' +
+              this.kitties[i].clicks +
+              '</span>' +
+              this.kitties[i].name +
+              '</li>'
+          );
+        }
 
-      $(".kitty").click(function(e) {
-        let catId = parseInt(this.id.substr(-1));
-        octopus.setActiveKitty(catId);
-      });
+        $(".kitty").click(function(e) {
+          let catId = parseInt(this.id.substr(-1));
+          octopus.setActiveKitty(catId);
+        });
+
+        this.resetButton.click(function(e) {
+          octopus.resetVotes();
+        });
+      }
 
     },
 
     updateBadge: function(id, votes) {
       $('#kitty'+id+' span.badge').text(votes);
+    },
+
+    enableReset: function() {
+      this.resetButton.removeClass('disabled');
+    },
+    disableReset: function() {
+      this.resetButton.addClass('disabled');
     }
   };
 
@@ -129,6 +151,9 @@ $(document).ready(function() {
       this.log('model initialized: ' + this.getKittiesNumber() + ' kittens found');
       catListView.init();
       this.log('catListView initialized');
+      if (this.getKittiesNumber() > 0) {
+        catListView.enableReset();
+      }
       catView.init();
       this.log('catView initialized');
     },
@@ -153,6 +178,9 @@ $(document).ready(function() {
       model.activeKitty = catId;
       catView.showActiveKitty(catId);
     },
+    showActiveKitty: function() {
+      catView.showActiveKitty(model.activeKitty);
+    },
 
     voteForKitty: function() {
       let kitty = this.getCurrentKitty();
@@ -161,6 +189,21 @@ $(document).ready(function() {
       //catView.showKitty(kitty);
       catListView.updateBadge(this.getCurrentKittyId(), kitty.clicks);
       catView.updateBadge(kitty.clicks);
+      this.saveVotes();
+    },
+
+    saveVotes: function() {
+      localStorage.setItem(model.lsName, JSON.stringify(this.getKitties()));
+    },
+
+    resetVotes: function() {
+      model.kitties.forEach(function(kat) {
+        kat.clicks = 0;
+      });
+      localStorage.setItem(model.lsName, JSON.stringify(this.getKitties()));
+      this.log('votes have been reset!');
+      catListView.show();
+      this.showActiveKitty();
     },
 
     log: function(s) {
